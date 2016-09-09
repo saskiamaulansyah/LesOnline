5ffb81faf9615b938595312cae9eaa70c728af5f
SaskiaMaulansyah <saskiamaulansyah@gmail.com>
2016-09-09 21:25:00 +0700

    second commit :Tampilan Awal

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4030/lesonline/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4030/lesonline/MainActivity.java
@@ -3,6 +3,7 @@ package id.sch.smktelkom_mlg.tugas01.xirpl4030.lesonline;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
